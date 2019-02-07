7f9248bcb7a6f1e59cff5bb7047e0cba16bc0bf7
Dustin Godbay <godbayd@aol.com>
2019-02-07 16:04:16 -0500

    in deploy commit

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 console.log('well done, this is your first site deployment in over a decade');
 console.log('updated');
+console.log('updating from develop branch which is to be merged before this deploys');
